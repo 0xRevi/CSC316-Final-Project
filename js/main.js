a16641f994e0cfe6f293d42c4d598118784d15f9
Jenny Oh <yjoh218964@gmail.com>
2025-03-05 15:24:04 -0500

    Adding bubble chart and bar chart

--- js/main.js
+++ js/main.js
@@ -1,25 +1,18 @@
-/* * * * * * * * * * * * * *
- *           MAIN           *
- * * * * * * * * * * * * * */
+// Instantiate and render the bubble chart
+const bubbleChart = new BubbleChart({
+    container: "#chart",
+    dataPath: "data/vis1.csv",
+    margin: 50
+});
+bubbleChart.render();
 
-function updateAllVisualizations() {
-    myPieChart.wrangleData();
-    myMapVis.wrangleData();
-}
+// Instantiate and render the bar chart
+const barChart = new BarChart({
+    dataPath: "data/vis2.csv",
+    container: "#barChart",
+    width: 900,
+    height: 500,
+    margin: { top: 20, right: 20, bottom: 60, left: 60 }
+});
+barChart.render();
 
-// load data using promises
-let promises = [d3.json("data/chart_data.json")];
-
-Promise.all(promises)
-    .then(function (data) {
-        initMainPage(data);
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
-
-// initMainPage
-function initMainPage(data) {
-    // log data
-    const controller = new TimeController(data[0]);
-}
